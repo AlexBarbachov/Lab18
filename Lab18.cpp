cd8122db37ef6e38af86421e4c2713b57f86d7ae
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-10-03 15:55:18 -0700

    second

--- Lab18.cpp
+++ Lab18.cpp
@@ -2,7 +2,42 @@
 
 using namespace std;
 
+struct review
+{
+    double rating;
+    string comment;
+    review* next;
+
+};
+
+
+
 int main()
 {
-    
+    int choice;
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+    cout << "Choice: ";
+    cin >> choice;
+
+
+
 }
+
+
+// new head node
+void addHead(review*& head, double rating, const string& comment)
+{
+
+}
+
+
+// new tail node
+void addTail(review* head, double rating, const string& comment)
+{
+
+}
+
+
+
